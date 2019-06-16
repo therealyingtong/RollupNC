02125d0bd984d8e7ccd7edf1de5c73a5e8598304
Chih Cheng Liang <chihchengliang@gmail.com>
2019-06-17 02:01:05 +0800

    remove unused

--- test/circuits/test_circom.js
+++
@@ -1,9 +0,0 @@
-const compiler = require('circom');
-
-
-describe("circom", ()=>{
-    it("should compile", async()=>{
-        const cirDef = await compiler("snark_circuit/if-gadgets.circom")
-
-    })
-})
